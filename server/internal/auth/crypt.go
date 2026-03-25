6f5e4150796fb650bf3ad682ac21dfb17c5ece27
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-03-26 00:00:21 +0700

    feat: add hash password, compare hash, and generate lookup id helper

---
+++ server/internal/auth/crypt.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	gonanoid "github.com/matoous/go-nanoid/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+func CompareHash(password, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+}
+
+func generateLookupID() (string, error) {
+	return gonanoid.Generate("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-", 12)
+}
