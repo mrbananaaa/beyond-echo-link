d24f18e57f460a9fb6aba5f0ff045c500caa4614
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-03-25 12:52:00 +0700

    feat: change db transaction implementation

--- server/internal/db/tx.go
+++ server/internal/db/tx.go
@@ -3,24 +3,43 @@ package db
 import (
 	"context"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	queries "github.com/mrbananaaa/bel-server/internal/db/sqlc"
 )
 
-func WithTx(
-	ctx context.Context,
-	pool *pgxpool.Pool,
-	fn func(q *queries.Queries) error,
-) error {
-	tx, err := pool.Begin(ctx)
+type txKey struct{}
+
+type TxManager struct {
+	pool *pgxpool.Pool
+}
+
+func NewTxManager(pool *pgxpool.Pool) *TxManager {
+	return &TxManager{
+		pool: pool,
+	}
+}
+
+// Inject transaciton to context
+func injectTx(ctx context.Context, tx pgx.Tx) context.Context {
+	return context.WithValue(ctx, txKey{}, tx)
+}
+
+// Extract transaction from context
+func ExtractTx(ctx context.Context) (pgx.Tx, bool) {
+	tx, ok := ctx.Value(txKey{}).(pgx.Tx)
+	return tx, ok
+}
+
+func (tm *TxManager) WithTx(ctx context.Context, fn func(ctx context.Context) error) error {
+	tx, err := tm.pool.Begin(ctx)
 	if err != nil {
 		return err
 	}
-	defer tx.Rollback(ctx)
 
-	q := queries.New(tx)
+	ctx = injectTx(ctx, tx)
 
-	if err := fn(q); err != nil {
+	if err := fn(ctx); err != nil {
+		_ = tx.Rollback(ctx)
 		return err
 	}
 
