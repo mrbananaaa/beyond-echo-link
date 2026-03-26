9722d2f39dc47e7da5a3a3be96dcc9ad9d1e15d1
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-03-26 13:53:11 +0700

    feat: create validation packagee

---
+++ server/internal/validation/validator.go
@@ -0,0 +1,13 @@
+package validation
+
+import "github.com/go-playground/validator/v10"
+
+type Validator struct {
+	validate *validator.Validate
+}
+
+func New() *Validator {
+	return &Validator{
+		validate: validator.New(),
+	}
+}
