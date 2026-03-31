1fb7335d198cf748241afbb598b4cefdc9ced117
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-01 01:55:55 +0700

    feat: create auth middleware

--- server/internal/http/middlewares/auth.go
+++ server/internal/http/middlewares/auth.go
@@ -1,10 +1,76 @@
 package middlewares
 
 import (
+	"context"
+	"errors"
+	"log/slog"
 	"net/http"
+	"strings"
 
+	"github.com/mrbananaaa/bel-server/internal/apperror"
+	"github.com/mrbananaaa/bel-server/internal/auth"
+	"github.com/mrbananaaa/bel-server/internal/http/httpx"
+	"github.com/mrbananaaa/bel-server/internal/http/response"
 	"github.com/mrbananaaa/bel-server/internal/logger"
 )
+
+type AuthMiddleware struct {
+	authService *auth.AuthService
+	log         *slog.Logger
+}
+
+func NewAuthMiddleware(
+	authService *auth.AuthService,
+	log *slog.Logger,
+) *AuthMiddleware {
+	return &AuthMiddleware{
+		authService: authService,
+		log:         log,
+	}
+}
+
+func (m *AuthMiddleware) VerifyAccessToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			err := apperror.InvalidCredentials(apperror.TypeInfrastructure, "invalid token", errors.New("empty Authorization header"))
+			logger.ErrorEvent(m.log,
+				"token_validation_failed",
+				"empty Authorization header",
+				err,
+			)
+			response.Error(w, r, err)
+			return
+		}
+
+		authHeaderValues := strings.Split(authHeader, " ")
+		if len(authHeaderValues) != 2 || strings.ToLower(authHeaderValues[0]) != "bearer" {
+			err := apperror.InvalidCredentials(apperror.TypeInfrastructure, "invalid token", errors.New("invalid Authorization header"))
+			logger.ErrorEvent(m.log,
+				"token_validation_failed",
+				"invalid Authorization header",
+				err,
+			)
+			response.Error(w, r, err)
+			return
+		}
+
+		userID, err := m.authService.ValidateToken(authHeaderValues[1])
+		if err != nil {
+			logger.ErrorEvent(m.log,
+				"token_validation_failed",
+				err.Error(),
+				err,
+			)
+			response.Error(w, r, err)
+			return
+		}
+
+		ctx := logger.WithUserID(r.Context(), userID)
+		ctx = context.WithValue(ctx, httpx.UserIDKey{}, userID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
 
 func MockAuth() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
