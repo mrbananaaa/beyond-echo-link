8db38b3d34ad29d0807300b13a988766323bad92
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-03 19:47:41 +0700

    refactor: remove mock auth middleware

--- server/internal/infra/http/middlewares/auth.go
+++ server/internal/infra/http/middlewares/auth.go
@@ -77,19 +77,3 @@ func (m *AuthMiddleware) VerifyAccessToken(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
-func MockAuth() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userIDHeader := r.Header.Get("X-User-ID")
-
-			ctx := r.Context()
-
-			if userIDHeader != "" {
-				ctx = logger.WithUserID(ctx, userIDHeader)
-			}
-
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
