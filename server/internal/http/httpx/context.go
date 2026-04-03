142b583eaa53ce113e17b10ff7c9cedfca7e7d26
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-03 16:41:14 +0700

    feat: creating context key and context related helper

--- server/internal/http/httpx/context.go
+++ server/internal/http/httpx/context.go
@@ -1,3 +1,35 @@
 package httpx
 
+import (
+	"context"
+	"errors"
+
+	"github.com/mrbananaaa/bel-server/internal/apperror"
+)
+
 type UserIDKey struct{}
+
+type UserIDContext struct {
+	UserID string
+}
+
+func UserIDFromContext(ctx context.Context) string {
+	if userIDContext, ok := ctx.Value(UserIDKey{}).(*UserIDContext); ok {
+		return userIDContext.UserID
+	}
+
+	return ""
+}
+
+func SetUserIDContext(ctx context.Context, userID string) error {
+	if userIDContext, ok := ctx.Value(UserIDKey{}).(*UserIDContext); ok {
+		userIDContext.UserID = userID
+		return nil
+	}
+
+	return apperror.InvalidCredentials(
+		apperror.TypeInfrastructure,
+		"couldn't set userID context value",
+		errors.New("failed to set userID context value"),
+	)
+}
