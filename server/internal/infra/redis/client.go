bdaeacc2ab230f46032ebfd9455f7d778e50f23d
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-03 20:46:02 +0700

    feat: add Ping and GetWithExists

--- server/internal/infra/redis/client.go
+++ server/internal/infra/redis/client.go
@@ -30,6 +30,13 @@ func (r *Redis) Close() error {
 	return r.client.Close()
 }
 
+func (r *Redis) Ping(ctx context.Context) error {
+	ctx, cancel := r.withTimeout(ctx)
+	defer cancel()
+
+	return r.client.Ping(ctx).Err()
+}
+
 func (r *Redis) buildKey(parts ...string) string {
 	key := strings.Join(parts, ":")
 
@@ -55,7 +62,22 @@ func (r *Redis) Get(ctx context.Context, key string) (string, error) {
 	ctx, cancel := r.withTimeout(ctx)
 	defer cancel()
 
-	return r.client.Get(ctx, key).Result()
+	val, err := r.client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", nil
+	}
+	return val, err
+}
+
+func (r *Redis) GetWithExists(ctx context.Context, key string) (string, bool, error) {
+	ctx, cancel := r.withTimeout(ctx)
+	defer cancel()
+
+	val, err := r.client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", false, nil
+	}
+	return val, true, err
 }
 
 func (r *Redis) Delete(ctx context.Context, keys ...string) error {
