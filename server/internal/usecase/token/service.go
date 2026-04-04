8779a5ddee42d1cf7a5fc63319cec88b392bbfc6
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-04 23:46:09 +0700

    feat: add GenerateRefreshToken, GetUserIDFromRefreshToken, RotateRefreshToken, DeleteRefreshToken

--- server/internal/usecase/token/service.go
+++ server/internal/usecase/token/service.go
@@ -1,15 +1,18 @@
 package token
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"log/slog"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/mrbananaaa/bel-server/internal/apperror"
 	"github.com/mrbananaaa/bel-server/internal/infra/redis"
+	"github.com/mrbananaaa/bel-server/internal/logger"
 )
 
 type TokenService struct {
@@ -100,16 +103,124 @@ func (s *TokenService) ValidateJWT(tokenStr string) (string, error) {
 	return claims.UserID, nil
 }
 
-// GenerateRefreshToken generate random string with rand.Read
-func (s *TokenService) GenerateRefreshToken() (string, error) {
+// GenerateRefreshToken generate random string with rand.Read and store it on redis
+func (s *TokenService) GenerateRefreshToken(ctx context.Context, userID string) (string, error) {
+	l := getLogger(ctx)
+
 	b := make([]byte, 32)
 	_, err := rand.Read(b)
 	if err != nil {
-		return "", apperror.Internal(
+		err = apperror.Internal(
 			apperror.TypeBusiness,
 			fmt.Errorf("failed to generate random byte: %w", err),
 		)
+		logger.ErrorEvent(l,
+			"refreshtoken_generation_failed",
+			"failed to generate token from rand.Read",
+			err,
+		)
+
+		return "", err
+	}
+	refreshToken := hex.EncodeToString(b)
+
+	if err = s.tokenStore.SetRefreshToken(ctx, refreshToken, userID, 7*24*time.Hour); err != nil {
+		err := apperror.Internal(
+			apperror.TypeBusiness,
+			fmt.Errorf("failed to set refresh token: %w", err),
+		)
+		logger.ErrorEvent(l,
+			"refreshtoken_generation_failed",
+			"failed to save refresh token to redis",
+			err,
+		)
+
+		return "", err
 	}
 
-	return hex.EncodeToString(b), nil
+	return refreshToken, nil
+}
+
+func (s *TokenService) GetUserIDFromRefreshToken(ctx context.Context, token string) (string, error) {
+	l := getLogger(ctx)
+
+	userID, exists, err := s.tokenStore.GetUserIDByToken(ctx, token)
+	if err != nil {
+		err := apperror.Internal(
+			apperror.TypeBusiness,
+			fmt.Errorf("failed to get userID with token: %w", err),
+		)
+		logger.ErrorEvent(l,
+			"refreshtoken_find_failed",
+			"failed to get userID with token",
+			err,
+		)
+
+		return "", err
+	}
+
+	if !exists {
+		err := apperror.Internal(
+			apperror.TypeBusiness,
+			fmt.Errorf("token not exists"),
+		)
+		logger.ErrorEvent(l,
+			"refreshtoken_find_failed",
+			"token not exists",
+			err,
+		)
+
+		return "", err
+	}
+
+	return userID, nil
+}
+
+func (s *TokenService) RotateRefreshToken(
+	ctx context.Context,
+	oldToken string,
+	newToken string,
+	userID string,
+) error {
+	l := getLogger(ctx)
+
+	if err := s.tokenStore.RotateRefreshToken(ctx, oldToken, newToken, userID, 7*24*time.Hour); err != nil {
+		err := apperror.Internal(
+			apperror.TypeBusiness,
+			fmt.Errorf("failed to rotate refresh token to redis: %w", err),
+		)
+		logger.ErrorEvent(l,
+			"refreshtoken_rotation_failed",
+			"failed to rotate refresh token",
+			err,
+		)
+
+		return err
+	}
+
+	return nil
+}
+
+func (s *TokenService) DeleteRefreshToken(ctx context.Context, token string) error {
+	l := logger.FromContext(ctx)
+
+	if err := s.tokenStore.DeleteRefreshToken(ctx, token); err != nil {
+		err := apperror.Internal(
+			apperror.TypeBusiness,
+			fmt.Errorf("failed to delete refresh token: %w", err),
+		)
+		logger.ErrorEvent(l,
+			"refreshtoken_deletion_failed",
+			"failed to delete refresh token from redis",
+			err,
+		)
+
+		return err
+	}
+
+	return nil
+}
+
+func getLogger(ctx context.Context) *slog.Logger {
+	return logger.FromContext(ctx).With("domain", "token")
 }
