f00a3ab66997f9f64fcb4e2dc360942163b99efd
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-04 23:46:47 +0700

    refactor: implementing token service functionalities

--- server/internal/infra/http/handlers/auth/handler.go
+++ server/internal/infra/http/handlers/auth/handler.go
@@ -2,9 +2,11 @@ package auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/mrbananaaa/bel-server/internal/apperror"
+	"github.com/mrbananaaa/bel-server/internal/infra/http/httpx"
 	"github.com/mrbananaaa/bel-server/internal/infra/http/response"
 	"github.com/mrbananaaa/bel-server/internal/logger"
 	"github.com/mrbananaaa/bel-server/internal/usecase/auth"
@@ -54,14 +56,19 @@ func (h *AuthHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: refresh token
-
-	// accessToken, err := h.authService.GenerateAccessToken(user.ID)
 	accessToken, err := h.tokenService.GenerateJWT(user.ID.String())
 	if err != nil {
 		response.Error(w, r, err)
 		return
 	}
+
+	refreshToken, err := h.tokenService.GenerateRefreshToken(r.Context(), user.ID.String())
+	if err != nil {
+		response.Error(w, r, err)
+		return
+	}
+
+	httpx.SetRefreshTokenCookie(w, refreshToken)
 
 	response.JSON(w, http.StatusCreated, map[string]any{
 		"user":         user,
@@ -99,13 +106,107 @@ func (h *AuthHandler) SignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	refreshToken, err := h.tokenService.GenerateRefreshToken(r.Context(), user.ID.String())
+	if err != nil {
+		response.Error(w, r, err)
+		return
+	}
+
+	httpx.SetRefreshTokenCookie(w, refreshToken)
+
 	response.JSON(w, http.StatusOK, map[string]any{
 		"user_id":      user.ID,
 		"access_token": accessToken,
 	})
 }
 
-// RefreshToken /auth/refresh
-func (h *AuthHandler) RefreshToken(w http.ResponseWriter, r *http.Request) {
+// SignOut /auth/signout
+// FIX: use userID from context to delete token
+func (h *AuthHandler) SignOut(w http.ResponseWriter, r *http.Request) {
+	l := logger.FromContext(r.Context())
 
+	refreshToken, err := httpx.GetRefreshTokenCookie(r)
+	if err != nil {
+		err := apperror.Internal(
+			apperror.TypeInfrastructure,
+			fmt.Errorf("failed to get refresh token from cookie: %v", err),
+		)
+		logger.ErrorEvent(l,
+			"auth_signingout_failed",
+			"failed to get refresh token from cookie",
+			err,
+		)
+
+		response.Error(w, r, err)
+		return
+	}
+
+	if err := h.tokenService.DeleteRefreshToken(r.Context(), refreshToken); err != nil {
+		response.Error(w, r, err)
+		return
+	}
+
+	httpx.ClearRefreshTokenCokie(w)
+
+	response.OK(w, map[string]any{
+		"status": "ok",
+	})
+}
+
+// Refresh /auth/refresh
+func (h *AuthHandler) Refresh(w http.ResponseWriter, r *http.Request) {
+	l := logger.FromContext(r.Context())
+
+	refreshToken, err := httpx.GetRefreshTokenCookie(r)
+	if err != nil {
+		err := apperror.InvalidCredentials(
+			apperror.TypeInfrastructure,
+			"invalid refresh token",
+			fmt.Errorf("failed to get refresh token from cookie: %w", err),
+		)
+		logger.ErrorEvent(l,
+			"auth_refreshtoken_failed",
+			"failed to get refresh token from cookie",
+			err,
+		)
+
+		response.Error(w, r, err)
+		return
+	}
+
+	// TODO: use userID from context instead
+	userID, err := h.tokenService.GetUserIDFromRefreshToken(r.Context(), refreshToken)
+	if err != nil {
+		response.Error(w, r, err)
+		return
+	}
+
+	accessToken, err := h.tokenService.GenerateJWT(userID)
+	if err != nil {
+		logger.ErrorEvent(l,
+			"refreshtoken_rotation_failed",
+			"failed to issuing new access token",
+			err,
+		)
+
+		response.Error(w, r, err)
+		return
+	}
+
+	newRefreshToken, err := h.tokenService.GenerateRefreshToken(r.Context(), userID)
+	if err != nil {
+		response.Error(w, r, err)
+		return
+	}
+
+	if err := h.tokenService.RotateRefreshToken(r.Context(), refreshToken, newRefreshToken, userID); err != nil {
+		response.Error(w, r, err)
+		return
+	}
+
+	httpx.SetRefreshTokenCookie(w, newRefreshToken)
+
+	response.OK(w, map[string]any{
+		"access_token": accessToken,
+	})
 }
