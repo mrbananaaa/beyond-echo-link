6131e172eb037f92fd016b7d85075bddbc345a2d
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-04 23:44:09 +0700

    refactor: change cookie parameter for development purpose

--- server/internal/infra/http/httpx/cookies.go
+++ server/internal/infra/http/httpx/cookies.go
@@ -1,12 +1,25 @@
 package httpx
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // TODO: refactor this to use env vars
 var (
-	refreshTokenAge        = 60 * 60 * 24 * 30
-	refreshTokenCookieName = "tokennamegng"
+	refreshTokenCookieName = "auth_refresh_token"
 )
+
+func SetRefreshTokenCookie(w http.ResponseWriter, token string) {
+	cookie := &http.Cookie{
+		Name:     refreshTokenCookieName,
+		Value:    token,
+		HttpOnly: true,
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+		Path:     "/",
+	}
+	http.SetCookie(w, cookie)
+}
 
 func GetRefreshTokenCookie(r *http.Request) (string, error) {
 	cookie, err := r.Cookie(refreshTokenCookieName)
@@ -14,4 +27,16 @@ func GetRefreshTokenCookie(r *http.Request) (string, error) {
 		return "", err
 	}
 	return cookie.Value, nil
+}
+
+func ClearRefreshTokenCokie(w http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:     refreshTokenCookieName,
+		Value:    "",
+		HttpOnly: true,
+		Secure:   false,
+		Path:     "/",
+		MaxAge:   -1,
+	}
+	http.SetCookie(w, cookie)
 }
