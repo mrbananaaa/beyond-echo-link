664c05ac6f82344e2b58a8a59814f84a0127b1c7
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-12 08:17:00 +0700

    feat: add read pump and write pump

--- server/internal/infra/websocket/client.go
+++ server/internal/infra/websocket/client.go
@@ -1,6 +1,14 @@
 package websocket
 
-import ws "github.com/coder/websocket"
+import (
+	"context"
+	"fmt"
+
+	ws "github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
+	"github.com/mrbananaaa/bel-server/internal/domain/chat"
+	"github.com/mrbananaaa/bel-server/internal/logger"
+)
 
 type Client struct {
 	ID   string
@@ -8,4 +16,67 @@ type Client struct {
 	Send chan []byte
 
 	Rooms map[string]bool
+}
+
+func (c *Client) ReadPump(ctx context.Context, hub *Hub) {
+	defer func() {
+		hub.Unregister <- c
+		c.Conn.Close(ws.StatusNormalClosure, "closed")
+	}()
+
+	l := logger.FromContext(ctx)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			var msg chat.Message
+			err := wsjson.Read(ctx, c.Conn, &msg)
+			if err != nil {
+				logger.ErrorEvent(l,
+					"msg_decode_failed",
+					"err decoding websocket message",
+					fmt.Errorf("couldn't decode websocket message: %w", err),
+				)
+				return
+			}
+
+			switch msg.Type {
+			case chat.JoinRoom:
+				hub.JoinRoom <- JoinRequest{
+					Client: c,
+					RoomID: msg.RoomID,
+				}
+			case chat.LeaveRoom:
+				hub.LeaveRoom <- LeaveRequest{
+					Client: c,
+					RoomID: msg.RoomID,
+				}
+			case chat.MessageText:
+				hub.Broadcast <- BroadcastRequest{
+					RoomID: msg.RoomID,
+					Data:   msg.Payload,
+				}
+			}
+		}
+	}
+}
+
+func (c *Client) WritePump(ctx context.Context) {
+	for {
+		select {
+		case msg, ok := <-c.Send:
+			if !ok {
+				return
+			}
+
+			err := wsjson.Write(ctx, c.Conn, msg)
+			if err != nil {
+				return
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
 }
