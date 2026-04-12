ce6fe1d9064937c34d91b2f94a2b4781abc93a42
mrbananaaa <mailme.mrbananaaaid@gmail.com>
2026-04-12 08:17:34 +0700

    feat: add Run (hub loop)

--- server/internal/infra/websocket/hub.go
+++ server/internal/infra/websocket/hub.go
@@ -27,3 +27,75 @@ type BroadcastRequest struct {
 	RoomID string
 	Data   []byte
 }
+
+func NewHub() *Hub {
+	return &Hub{
+		Clients:    make(map[string]*Client),
+		Rooms:      make(map[string]map[string]*Client),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		JoinRoom:   make(chan JoinRequest),
+		LeaveRoom:  make(chan LeaveRequest),
+		Broadcast:  make(chan BroadcastRequest),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.Register:
+			h.Clients[client.ID] = client
+		case client := <-h.Unregister:
+			delete(h.Clients, client.ID)
+
+			for roomID := range client.Rooms {
+				h.removeFromRoom(client, roomID)
+			}
+
+			close(client.Send)
+
+		case req := <-h.JoinRoom:
+			h.addToRoom(req.Client, req.RoomID)
+
+		case req := <-h.LeaveRoom:
+			h.removeFromRoom(req.Client, req.RoomID)
+
+		case msg := <-h.Broadcast:
+			h.broadcastToRoom(msg.RoomID, msg.Data)
+		}
+	}
+}
+
+func (h *Hub) addToRoom(c *Client, roomID string) {
+	if _, ok := h.Rooms[roomID]; !ok {
+		h.Rooms[roomID] = make(map[string]*Client)
+	}
+
+	h.Rooms[roomID][c.ID] = c
+	c.Rooms[roomID] = true
+}
+
+func (h *Hub) removeFromRoom(c *Client, roomID string) {
+	if room, ok := h.Rooms[roomID]; ok {
+		delete(room, c.ID)
+	}
+
+	delete(c.Rooms, roomID)
+}
+
+func (h *Hub) broadcastToRoom(roomID string, data []byte) {
+	room, ok := h.Rooms[roomID]
+	if !ok {
+		return
+	}
+
+	for _, client := range room {
+		select {
+		case client.Send <- data:
+		default:
+			// slow client → drop
+			close(client.Send)
+			delete(h.Clients, client.ID)
+		}
+	}
+}
